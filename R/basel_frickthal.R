6cc00476f00579f401d01179542cf3cf78adc5a7
Konstantinos Vantas <kon.vantas@gmail.com>
2025-08-27 11:35:33 +0300

    add the basel a 1798 Map data of the Basel and Frickthal Region

---
+++ R/basel_frickthal.R
@@ -0,0 +1,60 @@
+#' @title The 1798 Map data of the Basel and Frickthal Region
+#' @description A dataset of 343 Control Points (CPs) based on the sample dataset
+#'   from the MapAnalyst distortion analysis software. It's ideal for analyzing
+#'   complex, non-linear distortions.
+#'
+#' @details
+#' This dataset is derived from the sample data provided with the MapAnalyst
+#' software (<http://mapanalyst.cartography.ch>) in
+#' https://github.com/mclaeysb/distortionAnalysis/tree/master/data_sample.
+#'
+#' It is a list that consists of an `gcp` object with 343 control points and
+#' an `sf` object with a grid of 96 lines that represent the map in a vector
+#' format.
+#'
+#' An old map to analyze, named "Die Landschaft Basel und das Frickthal -
+#' entworfen und mit beweglichen Typen gesetzt von W. Haas". It shows part of
+#' northern Switzerland and was produced in 1798 by W. Haas.
+#'
+#' previous analysis revealed: (a) a west-toeast scale variation from
+#' approximately 1:175,000 to 1:190,000; and (b) significant distortions in the
+#' south-eastern and northwestern corners
+#'
+#' This makes the dataset an excellent test case for evaluating the ability of
+#' non linear models like `gam` to model and correct these challenging error
+#' patterns, which a simple `helmert` or `lm` model would not be able to
+#' address.
+#'
+#' @format An list with one `gcp` object (gcp) and one `sf` object (grid):
+#' \describe{
+#'   \item{gcp}{An `sf` object with 343 features and 6 variables:
+#'     \describe{
+#'       \item{source_x}{Numeric. The X-coordinate on the source map (already globally aligned).}
+#'       \item{source_y}{Numeric. The Y-coordinate on the source map (already globally aligned).}
+#'       \item{target_x}{Numeric. The X-coordinate on the reference map.}
+#'       \item{target_y}{Numeric. The Y-coordinate on the reference map.}
+#'       \item{dx}{Numeric. The residual difference in X (target_x - source_x).}
+#'       \item{dy}{Numeric. The residual difference in Y (target_y - source_y).}
+#'     }
+#'   }
+#'   \item{grid}{An `sf` object representing the map grid with 96 features
+#'   (lines).}
+#' }
+#' @source Data originally provided as a sample dataset for the MapAnalyst
+#'   distortion analysis software. See <http://mapanalyst.cartography.ch> and
+#'   https://github.com/mclaeysb/distortionAnalysis/tree/master/data_sample.
+#'
+#' @examples
+#' library(mapAI)
+#'
+#' # Load the dataset
+#' data(basel_frickthal)
+#'
+#' # view what data contains
+#' basel_frickthal$gcp
+#' basel_frickthal$grid
+#'
+#' # plot old map distrortions
+#' plot(basel_frickthal$gcp)
+#'
+"basel_frickthal"
--- data_raw/create_swiss_data.R
+++ data_raw/create_swiss_data.R
@@ -2,56 +2,58 @@ library(mapAI)
 library(sf)
 
 #crs = 21781
-swiss_p <- read_gcps("./data_raw/mclaeysb.csv")
+data_file <- read.csv("./data_raw/mclaeysb.csv")
+swiss_p <- read_gcp(source_x = data_file$source_x,
+                    source_y = data_file$source_y,
+                    target_x = data_file$target_x,
+                    target_y = data_file$target_y)
+rm(data_file)
 
 # initial displacement
-plot_displacement(swiss_p)
+plot(swiss_p)
 
 # fit a helmert model
-helmert_model <- train_pai_model(swiss_p, pai_method = "helmert")
+helmert_model <- train_pai_model(gcp_data = swiss_p, method = "helmert")
+
 
 # align orignal data using the helmert model
-swiss_align <- apply_pai_model(helmert_model, swiss_p)
+swiss_align <- predict(helmert_model, swiss_p)
 
 
-head(swiss_p)
-head(swiss_align)
+# replace original coords with transformed ones
+swiss_p$source_x <- swiss_align$target_x
+swiss_p$source_y <- swiss_align$target_y
 
-# compute again dx and dy for the aligned maps
-align_coords <- sf::st_coordinates(swiss_align)
+swiss_p$dx <- swiss_p$target_x - swiss_p$source_x
+swiss_p$dy <- swiss_p$target_y - swiss_p$source_y
+plot(swiss_p)
+rm(swiss_align)
 
-swiss_align$source_x <- align_coords[,1]
-swiss_align$source_y <- align_coords[,2]
-swiss_align$dx <- swiss_align$target_x - swiss_align$source_x
-swiss_align$dy <- swiss_align$target_y - swiss_align$source_y
+# create a grid of the source map
+
+# Create a regular line grid to serve as our reference map
+swiss_cps <- sf::st_as_sf(swiss_p, coords = c("source_x", "source_y"), crs = 21781)
+
+source_grid <- sf::st_make_grid(swiss_cps, n = c(12, 8), square = TRUE) |>
+  st_cast("MULTILINESTRING") |>
+  st_sf()
+plot(source_grid)
+
+# add to swiss data source_grid the
+
+basel_frickthal <- list(gcp = swiss_p, grid = source_grid)
+
+usethis::use_data(basel_frickthal, overwrite = TRUE)
 
 
-plot_displacement(swiss_align)
+#########
 
-swiss_cps <- swiss_align
+pai_model <- train_pai_model(basel_frickthal$gcp, "tps")
+surface(pai_model)
 
-usethis::use_data(swiss_cps, overwrite = TRUE)
+distortion_results <- analyze_distortion(pai_model)
 
+plot(distortion_results, metric = "area_scale")
 
-##########
-
-pai_model <- train_pai_model(swiss_cps, pai_method = "lm")
-
-
-# Analyze the distortion on a regular grid of points
-analysis_grid <- sf::st_make_grid(swiss_cps, n = c(20, 20)) %>%
-  sf::st_centroid() %>%
-  sf::st_sf()
-
-distortion_results <- analyze_distortion(pai_model, analysis_grid)
-
-# Plot the learned 'log2_area_scale'. This is a symmetric metric centered
-# at 0, making it ideal for a diverging palette. Red areas were expanded,
-# blue areas were contracted.
-plot_distortion_surface(
-  distortion_results,
-  metric = "log2_area_scale",
-  diverging = TRUE
-)
-
-plot_correction_surface(pai_model, swiss_cps)
+# plot tissots indicatrices
+indicatrices(distortion_results, scale_factor = 800)
